1b58152fd88260b6a303fc75f1494b82c0a79d05
zefirez <tsumirez@zoho.com>
2022-10-25 12:02:38 +0200

    upgrade to apple device detection script

--- scripts/mobile-app-download.js
+++ scripts/mobile-app-download.js
@@ -1,8 +1,10 @@
 // this script tries to detect an apple device and if it is one changes mobile app download link to apple app store version of it
 
 let downloadLink = document.querySelector("#mobile-app-splash > a");
-let isItSafari = window.navigator.userAgent.toLowerCase().indexOf("safari");
+let userAgent = window.navigator.userAgent;
+let isItApple = ((userAgent.indexOf(" Mac Os") +1) || (userAgent.indexOf(" Darwin")+1) || (userAgent.indexOf("iOS")+1));
 
-if(isItSafari+1) {
+
+if(isItApple) {
     downloadLink.setAttribute("href","https://apps.apple.com/us/app/zbiletem-bilety-komunikacji/id1028407437")
 }
