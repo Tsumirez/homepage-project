c0bb0ce3b358b212368f8a7186d54a66ad6a0673
zefirez <tsumirez@zoho.com>
2022-10-28 12:04:30 +0200

    Hamburger menu improvement: clicking link to city search box gives it focus.

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -2,9 +2,16 @@
 
 let hamburgerMenuCheckbox = document.querySelector('#hamburger-on-off');
 let hamburgerLinks = document.querySelectorAll('#hamburger-menu ul li a');
+let citySearchInput = document.querySelector("#city-search");
 
 hamburgerLinks.forEach(link => {
+    //when link is clicked, close hamburger menu
     link.addEventListener("click",()=> {
         hamburgerMenuCheckbox.checked = false;
     })
+
+    //if the link is for the search box, give it focus after clicking the link.
+    if(link.href.split("#").pop() =="search") {
+        link.addEventListener("click", () => setTimeout(() => citySearchInput.focus(),250))
+    }
 })
