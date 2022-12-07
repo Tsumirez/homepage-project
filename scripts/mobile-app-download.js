33827d1350cea42d5910e60169a54e70ce8ac670
zefirez <tsumirez@zoho.com>
2022-12-07 14:09:21 +0100

    Full page search & replace of mobile app download links to match device being used (if it's apple device)

--- scripts/mobile-app-download.js
+++ scripts/mobile-app-download.js
@@ -1,10 +1,21 @@
-// this script tries to detect an apple device and if it is one changes mobile app download link to apple app store version of it
+// this script tries to detect an apple device and if it is one changes all android app download links (save google play badge) to apple app store version of it
 
-let downloadLink = document.querySelector("#mobile-app-splash > a");
+//detect if the user agent is on apple device
 let userAgent = window.navigator.userAgent;
-let isItApple = ((userAgent.indexOf(" Mac OS") +1) || (userAgent.indexOf(" Darwin")+1) || (userAgent.indexOf("iOS")+1));
+let isItApple = ((userAgent.indexOf(" Mac OS") + 1) || (userAgent.indexOf(" Darwin") + 1) || (userAgent.indexOf("iOS") + 1));
 
 
-if(isItApple) {
-    downloadLink.setAttribute("href","https://apps.apple.com/us/app/zbiletem-bilety-komunikacji/id1028407437")
+//page links are by default for android devices. So if it's not apple device, there's nothing to do!
+if (isItApple) {
+
+    //get all links on page except badges
+    let nonBadgeLinks = document.querySelectorAll("a:not([id$='badge'])");
+
+    //detect if link is for dowloading mobile app and change it to app store version if it is
+    nonBadgeLinks.forEach(link => {
+        if (link.href == "https://play.google.com/store/apps/details?id=zbiletem.zbiletem") {
+            link.setAttribute("href", "https://apps.apple.com/us/app/zbiletem-bilety-komunikacji/id1028407437")
+        }
+    })
+
 }
