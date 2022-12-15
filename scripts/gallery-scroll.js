d065f647f20a1bf1da1b5bd5dd95b54458d16036
zefirez <tsumirez@zoho.com>
2022-12-15 14:19:24 +0100

    Hamburger menu visual upgrade

--- scripts/gallery-scroll.js
+++ scripts/gallery-scroll.js
@@ -5,20 +5,32 @@ let rightArrow = document.querySelector(".gallery > p:last-of-type");
 
 let scrollXDistance = imageContainer.offsetWidth;
 
-leftArrow.addEventListener('click',(e)=> {scroll(e, -scrollXDistance)});
-rightArrow.addEventListener('click',(e)=> {scroll(e, scrollXDistance)});
+leftArrow.addEventListener('click', (e) => { scroll(e, -scrollXDistance) });
+rightArrow.addEventListener('click', (e) => { scroll(e, scrollXDistance) });
 
 
 function scroll(e, scrollXDistance) {
-    imageContainer.scrollBy(scrollXDistance,0);
+    imageContainer.scrollBy(scrollXDistance, 0);
 }
 
 // "read more" functionality script
 
-let readMoreInputs = document.querySelectorAll("input.read-more");
-readMoreInputs.forEach(input => {
-    input.checked=false;
-    var realHeight = input.nextElementSibling.offsetHeight;
-    input.nextElementSibling.style.maxHeight = `${realHeight}px`;
-    input.checked = true;
-})
+function expandiblePanelsInit() {
+    let readMoreInputs = document.querySelectorAll("input.read-more");
+    readMoreInputs.forEach(input => {
+        let initialHeight = input.nextElementSibling.offsetHeight;
+        input.checked = false;
+        let realHeight = input.nextElementSibling.offsetHeight;
+
+        if (realHeight > (initialHeight + 27)) {
+            input.parentElement.style.paddingBottom = "6rem";
+            // alert (`${initialHeight} vs ${realHeight}`)
+        } else {
+            input.style.display = 'none'
+        }
+        input.nextElementSibling.style.maxHeight = `${realHeight}px`;
+        input.checked = true;
+    })
+}
+
+window.addEventListener("load", expandiblePanelsInit);
