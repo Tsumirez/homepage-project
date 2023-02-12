aca96f2ceda7a029db9f351bd4826d0c3522c6ef
zefirez <tsumirez@zoho.com>
2023-02-12 23:04:49 +0100

    New language selection menu added for both desktop and mobile

--- scripts/hamburger.js
+++ scripts/hamburger.js
@@ -4,6 +4,12 @@ let hamburgerMenuCheckbox = document.querySelector('#hamburger-menu');
 let hamburgerLinks = document.querySelectorAll('nav > a');
 let citySearchInput = document.querySelector("#city-search");
 
+//current language link highlight 
+let languageSelect = document.querySelectorAll("#lang a");
+let currentUrlSubDir = window.location.pathname.split("/").slice(-2,-1).pop();
+let defaultLangLink = document.querySelector("#lang a[href='./index.html']");
+
+
 hamburgerLinks.forEach(link => {
     //when link is clicked, close hamburger menu
     link.addEventListener("click",()=> {
@@ -18,3 +24,18 @@ let = searchLinks = document.querySelectorAll("a[href$='search'");
 searchLinks.forEach(link => {
     link.addEventListener("click", () => setTimeout(() => citySearchInput.focus(),250))
 });
+
+//this script highlights the current page language in language select drop menu
+let currentLangLink = defaultLangLink;
+let currentLanguageLabel = document.querySelector("#lang span");
+
+languageSelect.forEach(link => {
+    linkSubDir = link.getAttribute("href").split("/").slice(-2,-1);
+
+    if (currentUrlSubDir == linkSubDir) {
+        currentLangLink = link;
+    }
+})
+
+currentLanguageLabel.textContent = currentLangLink.textContent;
+currentLangLink.setAttribute("style", "color: var(--dark-blue)");
