81a183083fdd021cd5ff8f36b6585f824f1c3278
zefirez <tsumirez@zoho.com>
2023-02-22 08:55:15 +0100

    Mobile header Get app message now reacts to device type (apple vs android). Restored download link on the right.

--- scripts/mobile-app-download.js
+++ scripts/mobile-app-download.js
@@ -9,7 +9,7 @@ let isItApple = ((userAgent.indexOf(" Mac OS") + 1) || (userAgent.indexOf(" Darw
 if (isItApple) {
 
     //get all links on page except badges
-    let nonBadgeLinks = document.querySelectorAll("a:not([id$='badge'])");
+    let nonBadgeLinks = document.querySelectorAll("a:not([badge])");
 
     //detect if link is for dowloading mobile app and change it to app store version if it is
     nonBadgeLinks.forEach(link => {
@@ -17,5 +17,8 @@ if (isItApple) {
             link.setAttribute("href", "https://apps.apple.com/us/app/zbiletem-bilety-komunikacji/id1028407437")
         }
     })
+
+    let headerText = document.querySelector('#mobile-header-get-call span');
+    headerText.textContent = " the App Store";
 
 }
