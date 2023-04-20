51cd82b4dee9b881191f6e5bbc3fef45dca5f5e7
zefirez <tsumirez@zoho.com>
2023-04-20 17:38:13 +0200

    code change in fb plugin.js for testing

--- scripts/fb-plugin.js
+++ scripts/fb-plugin.js
@@ -1,6 +1,7 @@
 window.addEventListener('load', (e) => {
     var chatbox = document.getElementById('fb-customer-chat');
-    chatbox.setAttribute("page_id", "409602669206817");
+    //chatbox.setAttribute("page_id", "409602669206817"); //zbiletem fb page
+    chatbox.setAttribute("page_id", "111564121915694"); //test fb page
     chatbox.setAttribute("attribution", "biz_inbox");
     
     window.fbAsyncInit = function () {
